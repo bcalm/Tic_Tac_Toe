8a41d82b263d0e21f3e912bca71da541b8d3d948
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-05-19 18:39:21 +0530

    Updates

---
+++ precache-manifest.8cf38e82353068a1155f1a901a7cca62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3778b8c7303fd12ea5f869e57939409c",
+    "url": "/ttt/index.html"
+  },
+  {
+    "revision": "ea5cda4cb278ad9d1549",
+    "url": "/ttt/static/css/main.14a1b4b4.chunk.css"
+  },
+  {
+    "revision": "659c0a7a2e8c64e1027d",
+    "url": "/ttt/static/js/2.0c1511cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ttt/static/js/2.0c1511cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea5cda4cb278ad9d1549",
+    "url": "/ttt/static/js/main.0ef3f03d.chunk.js"
+  },
+  {
+    "revision": "af7fb0c3671baa7477bb",
+    "url": "/ttt/static/js/runtime-main.e99b2382.js"
+  }
+]);
